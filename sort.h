9e28f9e7e96a7578de4867947352a1c3b91b976f
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-08 13:38:56 +0200

    Add sort.h

--- sort.h
+++ sort.h
@@ -7,21 +7,18 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Points to the previous node
- * @next: Points to the next node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* sorting functions */
 void bubble_sort(int *array, size_t size);
 
 #endif
